43a535de7c268d07e9728e85a13451e7eb7782ee
Ali Akber Mehedi <aliakbermehedi@gmail.com>
2026-04-10 18:51:14 +0600

    Folder Structure - (api, components, hooks, lib -db,axios,auth), store-satte management

---
+++ src/components/candidate/ExamTimer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExamTimer = () => {
+  return (
+    <div>ExamTimer</div>
+  )
+}
+
+export default ExamTimer
---
+++ src/components/employer/CreateTestForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateTestForm = () => {
+  return (
+    <div>CreateTestForm</div>
+  )
+}
+
+export default CreateTestForm
